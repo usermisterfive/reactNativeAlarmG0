ab307f4294a85e592753db6bec3252b3aba3974d
usermisterfive <usermisterfive@yandex.ru>
2024-07-30 13:06:35 +0400

    #2. add volume up and down.

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useRef, useState } from 'react';
+import { Button, Image, Platform, StyleSheet, TextInput } from 'react-native';
+import { Audio } from 'expo-av';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  const [sound, setSound] = useState();
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [volume, setVolume] = useState();
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(require('./808.mp3'));
+    setSound(sound);
+    if (verifyVolume()) {
+      sound.setVolumeAsync(Number.parseFloat(volume));
+    }
+    await sound.playAsync();
+  }
+  async function stopSound() {
+    sound.unloadAsync();
+  }
+  function isAlarmTime() {
+    const date = new Date();
+    return (date.getHours() == hours
+      && date.getMinutes() == minutes);
+  }
+  async function setAlarm3() {
+    console.log("Hours=" + hours + ", Minutes="
+      + minutes + ", verifyTime()=" + verifyTime());
+    if (verifyTime()) {
+      const date1 = new Date();
+      date1.setHours(hours);
+      date1.setMinutes(minutes);
+      date1.setSeconds(0);
+      const date2 = new Date();
+      console.log("date1=" + date1 + ", date2=" + date2 + ", date1 - date2=" + (date1 - date2));
+      console.log("verifyVolume()=" + verifyVolume());
+      if (date1 > date2) {
+        setTimeout(() => playSound(), (date1 - date2));
+      }
+    }
+  }
+  function verifyTime() {
+    return hours >= 0 && hours <= 24 && minutes >= 0 && minutes <= 60
+  }
+  function verifyVolume() {
+    const volumeNumber = Number.parseFloat(volume);
+    return volumeNumber != NaN && volumeNumber >= 0 && volumeNumber <= 1;
+  }
+
+  return (
+    <ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText></ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Alarm set to: { hours }:{ minutes }</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Hours:</ThemedText>
+        <TextInput onChangeText={newText => setHours(newText)} style={styles.titleContainer} />
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Minutes:</ThemedText>
+        <TextInput onChangeText={newText => setMinutes(newText)} style={styles.titleContainer} />
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>Volume from 0.0 to 1.0:</ThemedText>
+        <TextInput onChangeText={newText => setVolume(newText)} style={styles.titleContainer} />
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <Button title="Set Alarm" onPress={setAlarm3}/>
+      </ThemedView>
+      <ThemedView style={styles.titleContainer}>
+        <Button title="Stop Sound" onPress={stopSound}/>
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
